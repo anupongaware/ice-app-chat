b940a8f0d1147eeb3c093768dc4543f029bf63e8
anupongaware <anupong.p@aware.co.th>
2021-08-31 11:53:00 +0700

    manage user with profile

--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,18 +1,15 @@
-import React from 'react'
+import React from 'react';
 import { Redirect, Route } from 'react-router';
+import { useProfile } from '../context/profile.context';
 
-const PublicRoute = ({children, ...routeProps}) => {
-    const profile = false;
+const PublicRoute = ({ children, ...routeProps }) => {
+  const profile = useProfile();
 
-    if(profile) {
-        return <Redirect to="/" />
-    }
+  if (profile) {
+    return <Redirect to="/" />;
+  }
 
-    return (
-        <Route {...routeProps}>
-            {children}
-        </Route>
-    )
-}
+  return <Route {...routeProps}>{children}</Route>;
+};
 
-export default PublicRoute
+export default PublicRoute;
