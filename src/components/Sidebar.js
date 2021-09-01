aa19d5f24df77a8897d71738cb0a155f58e8bda3
anupongaware <anupong.p@aware.co.th>
2021-09-01 14:25:39 +0700

    started building the sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DashboardToggle from './dashboard/DashboardToggle';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="h-100 pt-2">
+        <DashboardToggle />
+      </div>
+      bottom
+    </div>
+  );
+};
+
+export default Sidebar;
