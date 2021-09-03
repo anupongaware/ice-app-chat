958c0233cb1bcaa59644454c2d09099d8631e93d
anupongaware <anupong.p@aware.co.th>
2021-09-03 14:18:43 +0700

    create room context

--- src/rooms/RoomItem.js
+++ src/rooms/RoomItem.js
@@ -6,7 +6,7 @@ const RoomItem = () => {
     <div>
       <div className="d-flex justify-content-between align-items-center">
         <h3 className="text-disappear">Room name</h3>
-        <TimeAgo datatime={new Date()} className="font-normal text-black-45" />
+        <TimeAgo datetime={new Date()} className="font-normal text-black-45" />
       </div>
 
       <div className="d-flex align-items-center text-black-70">
