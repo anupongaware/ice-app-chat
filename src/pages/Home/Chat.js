2edca11fe706ec03308aefd4d8653b6cad318616
anupongaware <anupong.p@aware.co.th>
2021-09-03 16:23:09 +0700

    created layout for chat page

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Loader } from 'rsuite';
+import ChatBottom from '../../components/chat-window/button';
+import ChatTop from '../../components/chat-window/top';
+import Message from '../../components/chat-window/messages/index';
+import { useParams } from 'react-router';
+import { useRooms } from '../../context/rooms.context';
 
 const Chat = () => {
-  return <div>hello</div>;
+  const { chatId } = useParams();
+
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId);
+
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page"> Chat {chatId} not found </h6>;
+  }
+
+  return (
+    <>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+
+      <div className="chat-middle">
+        <Message />
+      </div>
+
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
 };
 
 export default Chat;
